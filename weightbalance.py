8f7f47376da1b0a3c6c7c66d467c7293293d460c
George Fahmy <geofahm@gmail.com>
2024-10-12 22:26:00 -0700

    update to weight and balance

--- weightbalance.py
+++ weightbalance.py
@@ -15,6 +15,9 @@ CABIN_SEATS_DIST = 97.48
 BAGGAGE_DIST = 126.78
 FWD_CG_LIMIT = 78.7
 AFT_CG_LIMIT = 86.82
+AFT_AEROBATIC_CG_LIMIT = 84.5
+MAX_WEIGHT_LIMIT = 1800
+CHORD_LENGTH = 58
 
 
 def sum(*args):
@@ -32,6 +35,10 @@ def divide(x, y):
     return x / y
 
 
+def calc_cg_percent_of_chord(cg):
+    return (cg - WING_LE) / CHORD_LENGTH * 100
+
+
 def calc_cg_percent(cg):
     return (cg - FWD_CG_LIMIT) / (AFT_CG_LIMIT - FWD_CG_LIMIT) * 100
 
@@ -44,61 +51,119 @@ def calc_cg(
     left_front_weight,
     right_front_weight,
     tailwheel_weight,
-    fuel_gal=0,
+    fuel_gal_start=0,
+    fuel_gal_use=0,
     pilot_weight=0,
     passenger_weight=0,
     baggage_weight=0,
+    chord=False,
 ):
-    fuel_weight = multiply(fuel_gal, 6)
-    weight = sum(
+    fuel_start_weight = multiply(fuel_gal_start, 6)
+    fuel_use_weight = multiply(fuel_gal_use, 6)
+
+    empty_weight = sum(
         left_front_weight,
         right_front_weight,
         tailwheel_weight,
-        fuel_weight,
+    )
+
+    start_weight = sum(
+        empty_weight,
+        fuel_start_weight,
         pilot_weight,
         passenger_weight,
         baggage_weight,
     )
-    moment = sum(
+
+    end_weight = sum(
+        empty_weight,
+        fuel_start_weight - fuel_use_weight,
+        pilot_weight,
+        passenger_weight,
+        baggage_weight,
+    )
+
+    empty_moment = sum(
         multiply(left_front_weight, FRONT_LEFT_DIST),
         multiply(right_front_weight, FRONT_RIGHT_DIST),
         multiply(tailwheel_weight, TAILWHEEL_DIST),
-        multiply(fuel_weight, FUEL_DIST),
+    )
+
+    start_moment = sum(
+        empty_moment,
+        multiply(fuel_start_weight, FUEL_DIST),
         multiply(pilot_weight, CABIN_SEATS_DIST),
         multiply(passenger_weight, CABIN_SEATS_DIST),
         multiply(baggage_weight, BAGGAGE_DIST),
     )
-    cg = divide(moment, weight)
-    cg_percent = calc_cg_percent(cg)
 
-    if weight > 1800:
-        raise f"Max Gross Weight {weight} must be less than 1800lb"
-
-    if 0 > cg_percent > 100:
-        raise f"CG {cg} outside CG envelope"
-
-    if fuel_gal > 42:
-        raise f"Fuel QTY {fuel_gal} exceeds max allowable of 42 gal"
-
-    return DotMap(
-        weight=f"{_round(weight)} lbs",
-        moment=f"{_round(moment)} in-lbs",
-        cg_location=f"{_round(cg)} inches",
-        cg_percent=f"{_round(cg_percent)}%",
+    end_moment = sum(
+        empty_moment,
+        multiply(fuel_start_weight - fuel_use_weight, FUEL_DIST),
+        multiply(pilot_weight, CABIN_SEATS_DIST),
+        multiply(passenger_weight, CABIN_SEATS_DIST),
+        multiply(baggage_weight, BAGGAGE_DIST),
     )
+    start_cg = divide(start_moment, start_weight)
+    if chord:
+
+        start_cg_percent = calc_cg_percent_of_chord(start_cg)
+    else:
+        start_cg_percent = calc_cg_percent(start_cg)
+
+    end_cg = divide(end_moment, end_weight)
+    if chord:
+        end_cg_percent = calc_cg_percent_of_chord(end_cg)
+    else:
+        end_cg_percent = calc_cg_percent(end_cg)
+
+    if any([start_weight, end_weight]) > MAX_WEIGHT_LIMIT:
+        raise f"Max Gross Weight {start_weight} must be less than 1800lb"
+
+    if 0 > any([start_cg_percent, end_cg_percent]) > 100:
+        raise "CG outside CG envelope"
+
+    if fuel_gal_start > 42:
+        raise f"Fuel QTY {fuel_gal_start} exceeds max allowable of 42 gal"
+
+    chord_limit = " of chord" if chord else " of CG envelope"
+
+    results = DotMap(
+        start_weight=f"{_round(start_weight)} lbs",
+        start_moment=f"{_round(start_moment)} in-lbs",
+        start_cg_location=f"{_round(start_cg)} inches",
+        start_cg_percent=f"{_round(start_cg_percent)}%{chord_limit}",
+    )
+
+    if fuel_gal_use:
+        results["end_weight"] = f"{_round(end_weight)} lbs"
+        results["end_moment"] = f"{_round(end_moment)} in-lbs"
+        results["end_cg_location"] = f"{_round(end_cg)} inches"
+        results["end_cg_percent"] = f"{_round(end_cg_percent)}%{chord_limit}"
+
+    return results
 
 
 input_flag = 1
 left_front_weight = 522.89
 right_front_weight = 522.89
 tailwheel_weight = 65.22
+fuel_gal_start = 0
+fuel_gal_use = 0
+pilot_weight = 0
+passenger_weight = 0
+baggage_weight = 0
+config_name = "Default"
+chord_flag = False
 
 if input_flag:
-    fuel_gal = input("Fuel Gallons: ") or 42
-    pilot_weight = input("Pilot Weight: ") or 210
-    passenger_weight = input("Passenger Weight: ") or 130
-    baggage_weight = input("Baggage Weight: ") or 97
-    config_name = input("Config name: ")
+    fuel_gal_start = input("Starting Fuel Gallons: ") or fuel_gal_start
+    fuel_gal_use = input("Fuel usage: ") or fuel_gal_use
+    pilot_weight = input("Pilot Weight: ") or pilot_weight
+    passenger_weight = input("Passenger Weight: ") or passenger_weight
+    baggage_weight = input("Baggage Weight: ") or baggage_weight
+    config_name = input("Config name: ") or config_name
+    chord_flag = bool(input("Chord Flag: "))
 
 result = DotMap(
     {
@@ -106,10 +171,12 @@ result = DotMap(
             left_front_weight=left_front_weight,
             right_front_weight=right_front_weight,
             tailwheel_weight=tailwheel_weight,
-            fuel_gal=int(fuel_gal),
+            fuel_gal_start=int(fuel_gal_start),
+            fuel_gal_use=int(fuel_gal_use),
             pilot_weight=int(pilot_weight),
             passenger_weight=int(passenger_weight),
             baggage_weight=int(baggage_weight),
+            chord=chord_flag,
         )
     }
 )
@@ -126,7 +193,7 @@ max_gross_weight = calc_cg(
     left_front_weight=left_front_weight,
     right_front_weight=right_front_weight,
     tailwheel_weight=tailwheel_weight,
-    fuel_gal=42,
+    fuel_gal_start=42,
     pilot_weight=210,
     passenger_weight=130,
     baggage_weight=97,
@@ -136,7 +203,7 @@ most_aft_cg = calc_cg(
     left_front_weight=left_front_weight,
     right_front_weight=right_front_weight,
     tailwheel_weight=tailwheel_weight,
-    fuel_gal=5,
+    fuel_gal_start=5,
     pilot_weight=210,
     passenger_weight=130,
     baggage_weight=97,
@@ -146,7 +213,7 @@ most_forward_cg = calc_cg(
     left_front_weight=left_front_weight,
     right_front_weight=right_front_weight,
     tailwheel_weight=tailwheel_weight,
-    fuel_gal=42,
+    fuel_gal_start=42,
     pilot_weight=210,
 )
 
@@ -154,7 +221,7 @@ first_flight_config = calc_cg(
     left_front_weight=left_front_weight,
     right_front_weight=right_front_weight,
     tailwheel_weight=tailwheel_weight,
-    fuel_gal=42,
+    fuel_gal_start=42,
     pilot_weight=210,
 )
 
