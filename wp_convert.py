339751932e6eb8a8976d632eae3574ab4bc7c505
George Fahmy <geofahm@gmail.com>
2025-01-03 15:52:49 -0800

    ug

--- wp_convert.py
+++ wp_convert.py
@@ -4,10 +4,45 @@ import re
 import xmltodict
 from bs4 import BeautifulSoup as bs
 from dotmap import DotMap
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer
+
+
+def flatten_dict(d, parent_key="", sep="_"):
+    items = []
+    for k, v in d.items():
+        if "index" in k:
+            continue
+        if "title" in k:
+            parent_key = v
+            continue
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
+def create_pdf(data, filename):
+    doc = SimpleDocTemplate(filename, pagesize=letter)
+    styles = getSampleStyleSheet()
+    story = []
+
+    flat_data = flatten_dict(data)
+
+    for key, value in flat_data.items():
+        if not value:
+            continue
+
+        story.extend((Paragraph(f"{key}: {value}", styles["Normal"]), Spacer(1, 12)))
+    doc.build(story)
+
 
 o = xmltodict.parse(
     open(
-        "/Users/gfahmy/Documents/projects/dynon/rv-7n890gf.wordpress.2024-11-05.000.xml",
+        "rv-7n890gf.wordpress.2025-01-03.000.xml",
         "rb",
     )
 )
@@ -59,7 +94,7 @@ for i, item in enumerate(d.content):
             clean_key = getpath(clean, item["wp:post_parent"])
             clean[clean_key[0]]["images"].append(item["wp:attachment_url"])
         except Exception:
-            print(item["wp:post_parent"])
+            # print(item["wp:post_parent"])
             continue
         images[item["wp:post_parent"]][item.title] = DotMap(
             index=i,
@@ -75,3 +110,5 @@ for key in clean.keys():
 
 with open("full.json", "w") as fp:
     json.dump(clean.toDict(), fp, indent=4, sort_keys=False)
+
+create_pdf(clean.toDict(), "output.pdf")
