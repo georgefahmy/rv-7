11d9d94e58aa2f1b8f4e934a2e7150d8be8cb6dd
George Fahmy <geofahm@gmail.com>
2026-02-17 14:08:39 -0800

    file path

--- scripts/dynon_analysis.py
+++ scripts/dynon_analysis.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
-raw = pd.read_csv(
-    "/Users/GFahmy/Desktop/RV-7/data_logs/2025-11-07-N890GF-SN35347-17.3.0.19825-TEST-USER_LOG_DATA-PART_05.csv"
-)
+raw = pd.read_csv(input("Full file path: "))
 raw = raw.fillna("0")
 data = raw[raw["GPS Date & Time"].str.contains("2025-11-06")]
 
